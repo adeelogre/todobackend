53afd8da6804bb252c4ec1894988555da8f597cc
adeel <adeel.ogre@gmail.com>
2021-02-01 14:51:08 +0000

    2nd part of the settings.py updated

--- src/todobackend/settings.py
+++ src/todobackend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS Settings
+
+CORS_ORIGIN_ALLOW_ALL = True
