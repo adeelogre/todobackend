d841c48987b3879203aa89135032d5cb6918d192
adeel <adeel.ogre@gmail.com>
2021-02-02 12:03:41 +0000

    Added serializers.py and updated views.py

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from todo.models import TodoItem
+from todo.serializers import TodoItemSerializer
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.reverse import reverse
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 # Create your views here.
+class TodoItemViewSet(viewsets.ModelViewSet):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
