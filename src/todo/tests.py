26938041eaad9d1487aeb3ea444e4763d5150824
Adeel <adeel.ogre@gmail.com>
2021-02-04 15:05:41 +0000

    Updated tests.py

--- src/todo/tests.py
+++ src/todo/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from todo.models import TodoItem
 
 # Create your tests here.
+def createItem(client):
+    url = reverse('todoitem-list')
+    data = {'title': 'Walk the dog'}
+    return client.post(url, data, format='json')
+
+class TestCreateTodoItem(APITestCase):
+    """
+    Ensure we can create a new todo item
+    """
